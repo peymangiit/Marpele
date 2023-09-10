518f9ed8fa0896e0bc74a047d02fb07cc2d8d6e4
Ali Aghajani <105543467+peymangiit@users.noreply.github.com>
2023-09-10 17:41:09 +0330

    map side

--- script.js
+++ script.js
@@ -25,5 +25,35 @@ buttonEl.addEventListener("click", () => {
         diceEl.innerHTML = `<div class="dice" id="dice">${getDiceFace()}</div>`;
         updateHistoryList();
         // console.log(updateHistoryList);
-    }, 1000);
+    },1000);
 });
+
+// drag pawn
+var dm = document.getElementById('dragme'); 
+dm.addEventListener('dragstart',drag_start,false); 
+document.body.addEventListener('dragover',drag_over,false); 
+document.body.addEventListener('drop',drop,false); 
+
+function drag_start(event) {
+    var style = window.getComputedStyle(event.target, null);
+    event.dataTransfer.setData("text/plain",
+    (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY));
+} 
+function drag_over(event) { 
+
+    event.preventDefault(); 
+    console.log("draged over");
+    return false; 
+} 
+function drop(event) { 
+    event.preventDefault();
+    console.log("droped");
+    var offset = event.dataTransfer.getData("text/plain").split(',');
+   
+    var dm = document.getElementById('dragme');
+    console.log(dm);
+    dm.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
+    dm.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
+    // dm.style.backgroundColor = "red";
+    return false;
+} 
